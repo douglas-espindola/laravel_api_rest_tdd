7e68c8baf8c57effefe680f3c5e9dcf4faea4218
Douglas Espindola <douglas.spindola15@gmail.com>
2023-11-20 00:04:43 -0300

    feat:implementado metodos show e store da api usando tdd

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BooksController extends Controller
 {
-    public function index(Book $book){
-        return response()->json($book->all());
+    /**
+     * @param Book $book
+     */
+    public function __construct(private Book $book){}
+
+    public function index(){
+        return response()->json($this->book->all());
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public  function show($id)
+    {
+        return response()->json($this->book->findOrfail($id));
+    }
+
+    public function store(Request $request){
+
+        return response()->json($this->book->create($request->all()), response::HTTP_CREATED);
     }
 }
